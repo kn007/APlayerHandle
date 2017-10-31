1651c654dc6f91b29e4f277b74fa5fe2f572f86a
Karl Chen <kn007@users.noreply.github.com>
2017-10-31 18:46:36 +0800

    Update class.aplayer.php

--- class.aplayer.php
+++ class.aplayer.php
@@ -3,7 +3,7 @@
 /**
 *	APlayer是一个美观大方、实用的HTML5播放器，由DIYgod编写，项目地址：https://github.com/MoePlayer/APlayer
 *
-*	调用APlayerHandle类并进行初始化，可以很简便的在WordPress使用上Aplayer
+*	调用APlayerHandle类并进行初始化，可以很简便的在WordPress使用上APlayer
 *	短代码调用方法详见博客文章：
 *
 *	支持原生[audio]标签，前提是增加title参数，并且使用src参数
@@ -15,12 +15,99 @@ class APlayerHandle {
 	protected static $instance = 0;
 
 	public function init() {
+		add_shortcode( 'aplayer', array( $this, 'playlist_shortcode' ) );
+		add_shortcode( 'aplayer_trac', array( $this, 'trac_shortcode' ) );
+		add_action( 'admin_print_footer_scripts', array( $this, 'shortcode_buttons' ), 99999 );
 	}
 
 	public function playlist_shortcode( $atts = array(), $content = '' ) {
+		if ( !is_singular() && !is_admin() ) return;
+
+		$this->instance++;
+		( 1 === $this->instance ) && wp_enqueue_script( 'aplayer', 'https://unpkg.com/aplayer', array(), "1.6.0", true );
+
+		$atts = shortcode_atts(
+			array(
+				'mutex'          => 'true',
+				'autoplay'       => 'true',
+				'theme'          => '#b7daff',
+				'preload'        => 'auto',
+				'mode'           => 'circulation',
+				'maxheight'      => '500',
+			), $atts, 'aplayer_shortcode' );
+
+		$atts['mutex']           = wp_validate_boolean( $atts['mutex'] );
+		$atts['autoplay']        = wp_validate_boolean( $atts['autoplay'] );
+		$atts['theme']           = esc_attr( $atts['theme'] );
+		$atts['preload']         = esc_attr( $atts['preload'] );
+		$atts['mode']            = esc_attr( $atts['mode'] );
+		$atts['maxheight']       = absint( $atts['maxheight'] );
+
+		$content                 = str_replace(PHP_EOL, '', strip_tags( nl2br( do_shortcode( $content ) ) ) );
+
+		if ( empty ( $content ) ) {
+			return;
+		} elseif ( FALSE !== ( $pos = strrpos( $content, ',' ) ) ) {
+			$content = substr_replace( $content, '', $pos, 1 );
+		}
+
+		$output = sprintf( '<script>var ap%u=new APlayer({element:document.getElementById("aplayer-%u"),mutex:%b,autoplay:%b,theme:"%s",preload:"%s",mode:"%s",listmaxheight:"%spx",music:[%s]});</script>',
+			$this->instance,
+			$this->instance,
+			$atts['mutex'],
+			$atts['autoplay'],
+			$atts['theme'],
+			$atts['preload'],
+			$atts['mode'],
+			$atts['maxheight'],
+			$content
+		);
+
+		$html = sprintf( '<div id="aplayer-%u" class="aplayer"></div>',
+			$this->instance
+		);
+
+		add_action( 'wp_footer', function () use ( $output ) {
+			echo '		' . $output . "\n";
+		}, 99999 );
+
+		return $html;
 	}
 
 	public function trac_shortcode( $atts = array(), $content = '' ) {
+		$atts = shortcode_atts(
+			array(
+				'title'    => '',
+				'author'   => '',
+				'src'      => '',
+				'pic'      => '',
+		), $atts, 'aplayer_trac_shortcode' );
+
+		$atts['title']     = sanitize_text_field( $atts['title'] );
+		$atts['author']    = sanitize_text_field( $atts['author'] );
+		$atts['src']       = esc_url_raw( $atts['src'] );
+		$atts['pic']       = esc_url_raw( $atts['pic'] );
+
+		if ( empty( $atts['title'] ) || empty( $atts['src'] ) ) return;
+
+		if ( empty( $atts['author'] ) ) $atts['author'] = 'Unknown';
+
+		if ( empty( $atts['pic'] ) ) {
+			$output = sprintf( '{title:"%s",author:"%s",url:"%s"}',
+				$atts['title'],
+				$atts['author'],
+				$atts['src']
+			);
+		} else {
+			$output = sprintf( '{title:"%s",author:"%s",url:"%s",pic:"%s"}',
+				$atts['title'],
+				$atts['author'],
+				$atts['src'],
+				$atts['pic']
+			);
+		}
+
+		return $output . ',';
 	}
 
 	public function shortcode_buttons() {
